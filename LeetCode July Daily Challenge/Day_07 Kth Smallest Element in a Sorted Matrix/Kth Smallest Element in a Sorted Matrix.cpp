1d2dc8ec3f9dffca5df5d2d0cd3a8b4989c9770e
GauravS1910 <19ucs246@lnmiit.ac.in>
2021-07-08 01:34:21 +0530

    Create Kth Smallest Element in a Sorted Matrix.cpp

---
+++ LeetCode July Daily Challenge/Day_07 Kth Smallest Element in a Sorted Matrix/Kth Smallest Element in a Sorted Matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& a, int k) {
+        int n = a.size(); 
+        priority_queue <int> q; 
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                q.push(a[i][j]); 
+            }
+        }
+        while(q.size() > k)
+        {
+            q.pop(); 
+        }
+        return q.top(); 
+    }
+};
